f04a77f75cf82911c1dd6d2c3367d41ed6ee83e4
Ihsan Sarwari <ihsar18@student.sdu.dk>
2020-04-17 13:15:17 +0200

    CreditHandler er lavet

---
+++ src/main/java/com/mycompany/creditsystem/persistence/CreditHandler.java
@@ -0,0 +1,26 @@
+package com.mycompany.creditsystem.persistence;
+
+import com.mycompany.creditsystem.domain.Credit;
+import com.mycompany.creditsystem.domain.ICreditHandler;
+
+public class CreditHandler implements ICreditHandler {
+    @Override
+    public Credit getCredit(int id) {
+        return null;
+    }
+
+    @Override
+    public void createCredit(Credit Credit) {
+
+    }
+
+    @Override
+    public void deleteCredit(int id) {
+
+    }
+
+    @Override
+    public void updateCredit(int id) {
+
+    }
+}
