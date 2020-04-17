3ab42c066d90d8f3219ec811aeae834659d0c015
Ihsan Sarwari <ihsar18@student.sdu.dk>
2020-04-17 13:10:43 +0200

    CreditHandler interface er lavet

--- src/main/java/com/mycompany/creditsystem/domain/ICreditHandler.java
+++ src/main/java/com/mycompany/creditsystem/domain/ICreditHandler.java
@@ -1,6 +1,6 @@
 package com.mycompany.creditsystem.domain;
 
-public interface CreditHandler {
+public interface ICreditHandler {
     public Credit getCredit(int id);
     public void createCredit (Credit Credit);
     public void deleteCredit (int id);
