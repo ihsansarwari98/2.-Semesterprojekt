58bfb4b366d26e2aee9e35cf15d0fe8bbacedb46
Svendpai <svvib19@student.sdu.dk>
2020-04-21 13:55:28 +0200

    ProductionCompanyHandler

--- src/main/java/com/mycompany/creditsystem/domain/interfaces/IProductionCompanyHandler.java
+++ src/main/java/com/mycompany/creditsystem/domain/interfaces/IProductionCompanyHandler.java
@@ -5,8 +5,8 @@ import com.mycompany.creditsystem.domain.logic.ProductionCompany;
 public interface IProductionCompanyHandler {
 
     public ProductionCompany getProductionCompany(int id);
-    public void createProductionCompany (String name, String password);
+    public boolean createProductionCompany (ProductionCompany productionCompany);
     public void deleteProductionCompany(int id);
     public void updateProductionCompany(int id);
-    
+
 }
--- src/main/java/com/mycompany/creditsystem/persistence/ProductionCompanyHandler.java
+++ src/main/java/com/mycompany/creditsystem/persistence/ProductionCompanyHandler.java
@@ -2,25 +2,63 @@ package com.mycompany.creditsystem.persistence;
 
 import com.mycompany.creditsystem.domain.interfaces.IProducerHandler;
 import com.mycompany.creditsystem.domain.logic.Producer;
+import com.mycompany.creditsystem.domain.interfaces.IProductionCompanyHandler;
+import com.mycompany.creditsystem.domain.logic.ProductionCompany;
 
-public class ProductionCompanyHandler implements IProducerHandler {
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ProductionCompanyHandler implements IProductionCompanyHandler {
     @Override
-    public Producer getProducer(int id) {
-        return null;
+    public ProductionCompany getProductionCompany(int id) {
+        try {
+
+            PreparedStatement stmt = ConnectionHandler.getInstance().getConnection().prepareStatement("SELECT * FROM production_companies WHERE production_company_id = ?");
+            stmt.setInt(1, id);
+
+            ResultSet sqlReturnValue = stmt.executeQuery();
+            //TODO: Tjek hvorvidt dette er måden at gøre det på
+            if (!sqlReturnValue.next()) {
+                return null;
+            }
+            return new ProductionCompany(sqlReturnValue.getString(1), sqlReturnValue.getString(2), sqlReturnValue.getString(3));
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
-    public boolean createProducer(Producer producer) {
-        return false;
+    public boolean createProductionCompany(ProductionCompany productionCompany) {
+
+        try {
+            // TODO: correct the parameters and ? values
+            PreparedStatement insertStatement = ConnectionHandler.getInstance().getConnection().prepareStatement("INSERT INTO production_companies (name, username, password) VALUES (?,?,?)");
+            // TODO: get correct things
+
+            insertStatement.setString(1, productionCompany.getName());
+            insertStatement.setString(2, productionCompany.getUsername());
+            insertStatement.setString(3, productionCompany.getPassword());
+
+
+            insertStatement.execute();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+
+    @Override
+    public void deleteProductionCompany(int id) {
+
     }
 
     @Override
-    public void deleteProducer(int id) {
-
-    }
-
-    @Override
-    public void updateProducer(int id) {
+    public void updateProductionCompany(int id) {
 
     }
 }
