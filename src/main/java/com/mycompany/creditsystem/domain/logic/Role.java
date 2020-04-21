76307189b748b016bced3f39086c18b572e0139b
Mustafa Ahmed <mustafano@live.dk>
2020-04-21 15:03:59 +0200

    Ændringer i ICreditHandler interface, CreditHandler. Oprydning i Credit klassen. Oprettelse af Role klasse Rettelser i Production og Production company (Lignende metoder).

---
+++ src/main/java/com/mycompany/creditsystem/domain/logic/Role.java
@@ -0,0 +1,27 @@
+package com.mycompany.creditsystem.domain.logic;
+
+public class Role {
+    private String title;
+    private int id;
+
+    public Role(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return title;
+    }
+}
